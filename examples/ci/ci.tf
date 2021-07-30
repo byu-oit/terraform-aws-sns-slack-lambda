27693f5f6001b30f818b8cf0586a163a4c0e42ab
Blake Morgan <blake_morgan@byu.edu>
2021-07-30 10:48:34 -0600

    Add missing variables

--- examples/ci/ci.tf
+++ examples/ci/ci.tf
@@ -15,6 +15,8 @@ module "ci_test" {
   private_subnet_ids        = module.acs.private_subnet_ids
   role_permissions_boundary = module.acs.role_permissions_boundary.arn
   slack_webhook_url         = "https://api.slack.com/FAKE/URL"
+  sns_topic_arns            = ["arn:aws:sns:us-west-2:977306314792:fake-sns-arn"]
+  vpc_id                    = module.acs.vpc.id
 }
 
 module "acs" {
