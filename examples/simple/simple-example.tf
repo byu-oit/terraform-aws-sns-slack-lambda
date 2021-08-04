995c93e942c5e0ea13ae55146fd8ebead8f9ac7c
Blake Morgan <blake_morgan@byu.edu>
2021-08-04 08:31:52 -0600

    Json (#4)
    * Add support for JSON mesasges
    
    * Update lambda

--- examples/simple/simple-example.tf
+++ examples/simple/simple-example.tf
@@ -4,6 +4,6 @@ provider "aws" {
 }
 
 module "sns_slack_lambda" {
-  source = "github.com/byu-oit/terraform-aws-sns-slack-lambda?ref=v1.0.1"
+  source = "github.com/byu-oit/terraform-aws-sns-slack-lambda?ref=v1.0.2"
   #source = "../" # for local testing during module development
 }
